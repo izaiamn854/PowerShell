31e593b7ee282d3bba9046fd1b75980cacb6ef51
Ismael Abdeslam Zaim Wadghiri Amnad <izaiamn854@g.educaand.es>
2025-11-20 14:15:13 +0100

    script

--- Ejercicio01.ps1
+++ Ejercicio01.ps1
@@ -21,28 +21,45 @@ Write-Host "La fila seleccionada es: $row"
 $nameService = $servSearch[$row].Name
 
 ""
-
+#Espere unos segundos
+""
+Write-Host "Esperando 5 segundos antes de reiniciar el servicio..."
+Start-Sleep -Seconds 5
+""
+#Parar el servicio
+""
 Write-Host "El nombre del servicio seleccionado es: $nameService"
 
 Stop-Service -Name $nameService
-
+""
+#Espere unos segundos
+""
+Write-Host "Esperando 5 segundos antes de reiniciar el servicio..."
+Start-Sleep -Seconds 5
+""
 #Mostrar estado del servicio
 ""
 Write-Host "El servicio $nameService se a parado. Estado actual:"
 
 Get-Service -Name $nameService
-
+""
 #Espere unos segundos
 ""
 Write-Host "Esperando 5 segundos antes de reiniciar el servicio..."
 Start-Sleep -Seconds 5
-
+""
 #Iniciar el servicio
 ""
 Write-Host "Reiniciando el servicio $nameService..."
 Start-Service -Name $nameService
-
+""
+#Espere unos segundos
+""
+Write-Host "Esperando 5 segundos antes de reiniciar el servicio..."
+Start-Sleep -Seconds 5
+""
 #Mostrar estado del servicio
 ""
 Write-Host "El servicio $nameService se ha reiniciado. Estado actual:"
 get-service -DisplayName *$dnservice*
+""
