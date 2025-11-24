d7541ebff2f649b99dd51df4ea57bcf9a0e429b2
Ismael Abdeslam Zaim Wadghiri Amnad <izaiamn854@g.educaand.es>
2025-11-24 14:15:17 +0100

    funciona

--- emancipado.ps1
+++ emancipado.ps1
@@ -2,8 +2,8 @@ param($path, $row)
 
 $personas = Import-Csv -Path $path -Delimiter ';'
 
-if ($personas[$row]. edad -ge 18) {
-    Write-Output "Es mayor de edad"
+if ($personas[$row].edad -ge 18) {
+    Write-Output "$($personas[$row].nombre) es mayor de edad"
 } else {
-    Write-Output "Es menor de edad"
+    Write-Output "$($personas[$row].nombre) es menor de edad" 
 }
