a268f366be55e2d015fe10e7fc049740d65b73e0
Ismael Abdeslam Zaim Wadghiri Amnad <izaiamn854@g.educaand.es>
2025-11-24 14:35:44 +0100

    last of today

--- emancipado.ps1
+++ emancipado.ps1
@@ -3,7 +3,7 @@ param($path, $row)
 $personas = Import-Csv -Path $path -Delimiter ';'
 
 if ($personas[$row].edad -ge 18) {
-    Write-Output "$($personas[$row].nombre) es mayor de edad"
+    Write-Output "$($personas[$row].nombre) $($personas[$row].primer_apellido) $($personas[$row].segundo_apellido) es mayor de edad tiene $($personas[$row].edad) años"
 } else {
-    Write-Output "$($personas[$row].nombre) es menor de edad" 
+    Write-Output "$($personas[$row].nombre) $($personas[$row].primer_apellido) $($personas[$row].segundo_apellido) es menor de edad tiene $($personas[$row].edad) años" 
 }
